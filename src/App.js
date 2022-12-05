01a65f10ef5b9dfdbb56b24782f6669e6f83fab9
lani-irving <laniirving@googlemail.com>
2022-12-05 14:31:17 +0000

    hedgehog

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from "./logo.svg";
+import hedgehog from "./hedgehog.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={hedgehog} className="App-logo" alt="logo" />
         <p>Hello world!</p>
         <a
           className="App-link"
